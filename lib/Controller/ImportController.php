60fa906f214ef6f3c1219211157849c16b02437c
Joas Schilling <coding@schilljs.com>
2017-08-26 16:46:46 +0200

    Import devices and miband data
    Signed-off-by: Joas Schilling <coding@schilljs.com>

--- lib/Controller/ImportController.php
+++ lib/Controller/ImportController.php
@@ -22,24 +22,29 @@
 namespace OCA\GadgetBridge\Controller;
 
 use Doctrine\DBAL\DBALException;
+use Doctrine\DBAL\Exception\UniqueConstraintViolationException;
 use OCP\AppFramework\Controller;
 use OCP\AppFramework\Http;
 use OCP\AppFramework\Http\DataResponse;
 use OCP\Files\File;
 use OCP\Files\IRootFolder;
 use OCP\Files\NotFoundException;
+use OCP\IDBConnection;
 use OCP\IRequest;
 use OCP\IUserSession;
 
 class ImportController extends Controller {
 
+	/** @var IDBConnection */
+	protected $connection;
 	/** @var IUserSession */
 	protected $userSession;
 	/** @var IRootFolder */
 	protected $rootFolder;
 
-	public function __construct($appName, IRequest $request, IUserSession $userSession, IRootFolder $rootFolder) {
+	public function __construct($appName, IRequest $request, IDBConnection $connection, IUserSession $userSession, IRootFolder $rootFolder) {
 		parent::__construct($appName, $request);
+		$this->connection = $connection;
 		$this->userSession = $userSession;
 		$this->rootFolder = $rootFolder;
 	}
@@ -82,6 +87,75 @@ class ImportController extends Controller {
 		}
 
 		$query = $connection->getQueryBuilder();
+		$query->automaticTablePrefix(false);
+		$query->select('*')
+			->from('DEVICE');
+
+		$insert = $this->connection->getQueryBuilder();
+		$insert->insert('gadgetbridge_devices')
+			->values([
+				'user' => $insert->createParameter('user'),
+				'device_id' => $insert->createParameter('device_id'),
+				'name' => $insert->createParameter('name'),
+				'manufacturer' => $insert->createParameter('manufacturer'),
+				'identifier' => $insert->createParameter('identifier'),
+				'type' => $insert->createParameter('type'),
+				'model' => $insert->createParameter('model'),
+			]);
+
+		$result = $query->execute();
+		while ($row = $result->fetch()) {
+			$insert->setParameter('user', $user->getUID())
+				->setParameter('device_id', (int) $row['_id'])
+				->setParameter('name', $row['NAME'])
+				->setParameter('manufacturer', $row['MANUFACTURER'])
+				->setParameter('identifier', $row['IDENTIFIER'])
+				->setParameter('type', (int) $row['TYPE'])
+				->setParameter('model', $row['MODEL']);
+			try {
+				$insert->execute();
+			} catch (UniqueConstraintViolationException $e) {
+				// Ignore
+			}
+		}
+		$result->closeCursor();
+
+		$query = $connection->getQueryBuilder();
+		$query->automaticTablePrefix(false);
+		$query->select('*')
+			->from('MI_BAND_ACTIVITY_SAMPLE');
+
+		$insert = $this->connection->getQueryBuilder();
+		$insert->insert('gadgetbridge_miband')
+			->values([
+				'user' => $insert->createParameter('user'),
+				'device_id' => $insert->createParameter('device_id'),
+				'user_id' => $insert->createParameter('user_id'),
+				'datetime' => $insert->createParameter('datetime'),
+				'raw_intensity' => $insert->createParameter('raw_intensity'),
+				'steps' => $insert->createParameter('steps'),
+				'raw_kind' => $insert->createParameter('raw_kind'),
+				'heart_rate' => $insert->createParameter('heart_rate'),
+			]);
+
+		$result = $query->execute();
+		while ($row = $result->fetch()) {
+			$insert->setParameter('user', $user->getUID())
+				->setParameter('device_id', (int) $row['DEVICE_ID'])
+				->setParameter('user_id', (int) $row['USER_ID'])
+				->setParameter('datetime', \DateTime::createFromFormat('U', (int) $row['TIMESTAMP']), 'datetime')
+				->setParameter('raw_intensity', (int) $row['RAW_INTENSITY'])
+				->setParameter('steps', (int) $row['STEPS'])
+				->setParameter('raw_kind', (int) $row['RAW_KIND'])
+				->setParameter('heart_rate', (int) $row['HEART_RATE']);
+			try {
+				$insert->execute();
+			} catch (UniqueConstraintViolationException $e) {
+				// FIXME use offset on select
+			}
+		}
+		$result->closeCursor();
+
 		$connection->close();
 
 		return new DataResponse();
